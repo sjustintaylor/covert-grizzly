44dc0362de8fc478a65f099232e54f260df384a0
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-09-13 17:56:55 +1000

    feat: checkpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/covert-grizzly/" : "/",
   build: {
     outDir: "dist",
   },
